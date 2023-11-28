95ea446e89e8e47391861c40ccaa503fb6518f06
sawyer bristol <sawyerbristol@gmail.com>
2023-11-28 17:22:58 +0000

    adding new flake

--- flake.nix
+++ flake.nix
@@ -1,119 +1,90 @@
 {
   description = "A 'snake game'";
 
+  # Flake inputs
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    nur.url = "github:polygon/nur.nix";
-    naersk.url = "github:nix-community/naersk";
+    # nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2305.491812.tar.gz";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay"; # A helper for Rust + Nix
+    nixgl.url = "github:guibou/nixGL"; # Allows you to run OpenGL and or Vulkan applications in a nix shell
   };
 
-  outputs = { self, rust-overlay, nixpkgs, nur, naersk }:
+  # Flake outputs
+  outputs = { self, nixpkgs, rust-overlay, nixgl, ... }:
     let
-      systems = [ "aarch64-linux" "i686-linux" "x86_64-linux" ];
-      overlays = [ (import rust-overlay) ];
-      program_name = "bevy_nix_vscode_template";
-    in builtins.foldl' (outputs: system:
+      # Overlays enable you to customize the Nixpkgs attribute set
+      overlays = [
+        # Makes a `rust-bin` attribute available in Nixpkgs
+        (import rust-overlay)
+        nixgl.overlay
+        # Provides a `rustToolchain` attribute for Nixpkgs that we can use to
+        # create a Rust environment
+        (self: super: {
+          rustToolchain = super.rust-bin.stable.latest.default;
+        })
+      ];
 
-      let
-        overlays = [ (import rust-overlay) ];
+      # Systems supported
+      allSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+
+      # Helper to provide system-specific attributes
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
         pkgs = import nixpkgs { inherit overlays system; };
-
-        rust-bin = pkgs.rust-bin.selectLatestNightlyWith (toolchain:
-          toolchain.default.override {
-            targets = [ "wasm32-unknown-unknown" ];
-            extensions = [ "rust-src" ];
-          });
-        naersk-lib = naersk.lib.${system}.override {
-          cargo = rust-bin;
-          rustc = rust-bin;
-        };
-
-        rust-dev-deps = with pkgs; [
-          cargo-deny
-          cargo-edit
-          cargo-watch
-          rustup
-          rust-analyzer
-          rustfmt
-          lldb
-          cargo-geiger
-          nur.packages.${system}.wasm-server-runner
-          renderdoc
-        ];
-        build-deps = with pkgs; [
-          pkgconfig
-          pkg-config
-          mold
-          clang
-          makeWrapper
-          lld
-        ];
-        runtime-deps = with pkgs; [
-          alsa-lib
-          alsa-tools
-          alsa-plugins
-          pipewire
-          udev
-          xorg.libX11
-          xorg.libXcursor
-          xorg.libXrandr
-          xorg.libXi
-          xorg.libxcb # To use the x11 feature
-          libxkbcommon
-          wayland # To use the wayland feature
-          libGL
-          vulkan-loader
-          vulkan-headers
-        ];
-      in {
-        devShells.${system}.default = let
-          all_deps = runtime-deps ++ build-deps ++ rust-dev-deps
-            ++ [ rust-bin ];
-        in pkgs.mkShell {
-          buildInputs = all_deps;
-          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (all_deps);
-          PROGRAM_NAME = program_name;
+      });
+    in
+    {
+      # Development environment output
+      devShells = forAllSystems ({ pkgs }: {
+        default =  pkgs.mkShell {
+          # The Nix packages provided in the environment
+          packages = (with pkgs; [
+            # Fluff
+            cargo-mommy
+            onefetch
+            # Bevy
+            pkg-config
+            alsa-lib
+            vulkan-tools
+            vulkan-headers
+            vulkan-loader
+            vulkan-validation-layers
+            udev
+            clang
+            lld
+             # If using an intel GPU
+             pkgs.nixgl.nixVulkanIntel
+             # If on x11
+             xorg.libX11
+             xorg.libX11
+             xorg.libXcursor
+             xorg.libXi
+             xorg.libXrandr
+             # If on wayland
+             libxkbcommon
+             wayland
+            # Rust
+            rustup
+            rustToolchain
+          ]) ++ pkgs.lib.optionals pkgs.stdenv.isDarwin (with pkgs; [ libiconv ]);
           shellHook = ''
-            export CARGO_MANIFEST_DIR=$(pwd)
-            export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${
-              pkgs.lib.makeLibraryPath (all_deps)
-            }"'';
+            # Required
+            export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath [
+              pkgs.alsaLib
+              pkgs.udev
+              pkgs.vulkan-loader
+            ]}"
+            # Aliases and other fluff/ease of use
+            alias runIntel="nixVulkanIntel cargo run"
+            alias runMommyIntel="nixVulkanIntel cargo mommy run"
+            onefetch
+            echo "Welcome to nix-hell uh nix-shell!"
+          '';
         };
-        packages.${system} = {
-          app = naersk-lib.buildPackage {
-            pname = program_name;
-            root = ./.;
-            buildInputs = runtime-deps;
-            nativeBuildInputs = build-deps;
-            overrideMain = attrs: {
-              fixupPhase = ''
-                wrapProgram $out/bin/${program_name} \
-                  --prefix LD_LIBRARY_PATH : ${
-                    pkgs.lib.makeLibraryPath runtime-deps
-                  } \
-                  --set CARGO_MANIFEST_DIR $out/share/bevy_nix_vscode_template
-                mkdir -p $out/share/${program_name}
-                cp -a assets $out/share/${program_name}'';
-              patchPhase = ''
-                sed -i s/\"dynamic\"// Cargo.toml
-              '';
-            };
-          };
-          wasm = self.packages.${system}.app.overrideAttrs (final: prev: {
-            CARGO_BUILD_TARGET = "wasm32-unknown-unknown";
-            fixupPhase = "";
-          });
-        };
-        defaultPackage.${system} = self.packages.${system}.app;
-        apps.${system}.wasm = {
-          type = "app";
-          program = "${pkgs.writeShellScript "wasm-run" "${
-              nur.packages.${system}.wasm-server-runner
-            }/bin/wasm-server-runner ${
-              self.packages.${system}.wasm
-            }/bin/${program_name}.wasm"}";
-        };
-      }) { } systems;
-
+      });
+    };
 }
